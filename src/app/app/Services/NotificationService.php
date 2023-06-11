4ac21a530dca865a1cd53846369656f575273c2c
Akito Koga <akito121215@gmail.com>
2023-06-11 19:15:38 +0900

    add: メッセージの形式を指定して送信するサービスクラスを実装

---
+++ src/app/app/Services/NotificationService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\LINEBotService;
+
+class NotificationService
+{
+    /**
+     * @var LINEBotService
+     */
+    protected $lineBotService;
+
+    /**
+     * @var string
+     */
+    protected $message = "昨日のメールを教えるね！昨日は--件のメールがきたよ！";
+
+    public function __construct(LINEBotService $service)
+    {
+        $this->lineBotService = $service;
+    }
+
+    /**
+     * Send email notifications to specific user
+     * @param string $userId
+     * @return void
+     */
+    public function sendNotificationByUserId($userId)
+    {
+        $this->lineBotService->sendLineMessageByUserId($this->message, $userId);
+    }
+
+    /**
+     * Send email notifications by multicast
+     */
+    public function sendNotificationByBroadcast()
+    {
+        $this->lineBotService->sendLineMessageByBroadcast($this->message);
+    }
+}
