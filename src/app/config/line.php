db011ec6967fc7ee075403b78c3c8b41dd8c6479
Akito Koga <akito121215@gmail.com>
2023-06-11 12:54:51 +0900

    add: LINE Messaging APIのシークレットキー読み込み部分の追加

---
+++ src/app/config/line.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'message' => [
+        'line_bot_channel_access_token' => env('LINE_BOT_CHANNEL_ACCESS_TOKEN', ''),
+        'line_bot_channel_secret' => env('LINE_BOT_CHANNEL_SECRET', ''),
+    ],
+];
