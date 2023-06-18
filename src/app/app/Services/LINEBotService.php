3a60ebc38e5c0c5d53969e1e96e8fc27dee60bc1
Akito Koga <akito121215@gmail.com>
2023-06-19 05:03:11 +0900

    refactor: チャンネルへの送信処理をメソッドとして移動

--- src/app/app/Services/LINEBotService.php
+++ src/app/app/Services/LINEBotService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Traits\SendLineMessageTrait;
+use Illuminate\Support\Facades\Http;
 
 class LINEBotService
 {
@@ -36,5 +37,29 @@ class LINEBotService
         ];
 
         $this->sendLineMessage($line_api_url, $body);
+    }
+
+    /**
+     * Send line message
+     *
+     * @param string $url
+     * @param array $body
+     * @return void
+     */
+    public function sendLineMessage(string $url, array $body)
+    {
+        $headers = [
+            'Authorization' => 'Bearer ' . config('line.message.line_bot_channel_access_token'),
+            'Content-Type'  => 'application/json',
+        ];
+
+        $response = Http::withHeaders($headers)->post($url, $body);
+
+        if ($response->getStatusCode() != 200) {
+            logger()->error('Failed to send message.', [
+                'status' => $response->getStatusCode(),
+                'body'   => $response->getBody(),
+            ]);
+        }
     }
 }
